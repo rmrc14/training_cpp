6664d6f026887a3f63a32db6cf2de64975c0f41f
rmrc14 <remin1459@gmail.com>
2025-04-23 18:04:15 +0530

    qs 4

---
+++ homework/day_07/qs4_4.cpp
@@ -0,0 +1,25 @@
+/*
+4) Isha is playing with numbers and trying to write program to Check if the sum of distinct digits of two integers are equal
+
+Output Format:
+If  the sum of distinct digits of both the numbers are equal
+if true	print YES
+otherwise  print NO.
+
+Examples:
+Input:
+ 2452
+ 9222
+Output:
+ YES
+
+Input
+121
+3035
+Output:
+ NO
+
+
+*/
+
+
