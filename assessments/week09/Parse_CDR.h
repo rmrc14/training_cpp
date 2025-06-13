b1af97c9acd1f6584bd352b503c25b4cecd4206b
rmrc14 <remin1459@gmail.com>
2025-06-13 17:54:33 +0530

    commit

--- assessments/week09/Parse_CDR.h
+++ assessments/week09/Parse_CDR.h
@@ -2,13 +2,16 @@
 
 #include "CoreHeader.h"
 #include "Customer.h"
-
+#include "Operator.h"
 
 class ParseCdr
 {
 	std::ifstream fin;
 
 	std::vector<CDR> vCdr;
+
+	std::vector<Customer> v_customer;
+	std::vector<Operator> o_operator;
 
 public:
 	ParseCdr(std::string filename)
@@ -48,6 +51,23 @@ void ParseCdr::seperateCustomer()
 			{
 				c.setOutgoing_SMS_messages_w(c.getOutgoing_SMS_messages_w() + a.Duration);
 			}
+			// for call
+			else if (a.Call_type == "MTC")
+			{
+				c.setIncoming_voice_call_durations_w(c.getIncoming_voice_call_durations_w() + a.Duration);
+			}
+			else if (a.Call_type == "MOC")
+			{
+				c.setOutgoing_voice_call_durations_w(c.getOutgoing_voice_call_durations_w() + a.Duration);
+			}
+			// for data
+			else if (a.Call_type == "GPRS")
+			{
+				c.setDownloaded_w(c.getDownloaded_w() + a.Download);
+				c.setUploaded_w(c.getUploaded_w() + a.Upload);
+			}
+
+
 		}
 		else
 		{
@@ -59,23 +79,7 @@ void ParseCdr::seperateCustomer()
 			{
 				c.setOutgoing_SMS_messages_o(c.getOutgoing_SMS_messages_o() + a.Duration);
 			}
-
-		}
-		// for call
-		if (a.Operator_MMC_MNC == a.Third_party_operator_MMC_MNC)
-		{
-			if (a.Call_type == "MTC")
-			{
-				c.setIncoming_voice_call_durations_w(c.getIncoming_voice_call_durations_w() + a.Duration);
-			}
-			else if (a.Call_type == "MOC")
-			{
-				c.setOutgoing_voice_call_durations_w(c.getOutgoing_voice_call_durations_w() + a.Duration);
-			}
-		}
-		else
-		{
-			if (a.Call_type == "MTC")
+			else if (a.Call_type == "MTC")
 			{
 				c.setIncoming_voice_call_durations_o(c.getIncoming_voice_call_durations_o() + a.Duration);
 			}
@@ -83,14 +87,18 @@ void ParseCdr::seperateCustomer()
 			{
 				c.setOutgoing_voice_call_durations_o(c.getOutgoing_voice_call_durations_o() + a.Duration);
 			}
+			else if (a.Call_type == "GPRS")
+			{
+				c.setDownloaded_o(c.getDownloaded_o() + a.Download);
+				c.setUploaded_o(c.getUploaded_o() + a.Upload);
+			}
 
+			v_customer.push_back(c);
 		}
-		// for data
-		if (a.Call_type == "GPRS")
-		{
-			c.setDownloaded_w(c.getDownloaded_w() + a.Download);
-			c.setUploaded_w(c.getUploaded_w() + a.Upload);
-		}
+		
+		
+		
+		
 
 	}
 
